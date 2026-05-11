fb6ceca7dd847b9518648b0cebc9da170339c86b
denpitbiker <46526801+denpitbiker@users.noreply.github.com>
2026-05-11 18:08:35 +0300

    Checkstyle...

--- server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.error.NotFoundException;
-import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestItemDto;
